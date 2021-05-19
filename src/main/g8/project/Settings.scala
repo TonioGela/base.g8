c6015076df2b175eb64c05c6374dc8c1f423753e
Antonio Gelameris <toniogela89@gmail.com>
2021-05-19 17:13:04 +0000

    Update scala-typed-holes to 0.1.9

--- src/main/g8/project/Settings.scala
+++ src/main/g8/project/Settings.scala
@@ -14,7 +14,7 @@ object Settings {
   private val compilerPlugins: List[ModuleID] = List(
     compilerPlugin("com.olegpy"              %% "better-monadic-for" % "0.3.1"),
     crossVersionFullPlugin("org.typelevel"    % "kind-projector"     % "0.13.0"),
-    crossVersionFullPlugin("com.github.cb372" % "scala-typed-holes"  % "0.1.8")
+    crossVersionFullPlugin("com.github.cb372" % "scala-typed-holes"  % "0.1.9")
   )
 
   private val scalaLangVersion: String = "2.13.5"
