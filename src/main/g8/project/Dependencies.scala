fb41ad2479e8a7d3595ffaf02a26659ff7656993
Antonio Gelameris <toniogela89@gmail.com>
2021-05-27 01:37:48 +0000

    Update ciris to 2.0.0

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val coreDependencies: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-effect"    % "3.1.1",
     "com.monovore"  %% "decline-effect" % "2.0.0",
-    "is.cir"        %% "ciris"          % "2.0.0-RC3"
+    "is.cir"        %% "ciris"          % "2.0.0"
   )
 
   val testDependencies: Seq[ModuleID] = Seq("org.typelevel" %% "munit-cats-effect-3" % "1.0.3").map(_ % Test)
