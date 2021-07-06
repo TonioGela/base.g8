66787609c258e70a26c552ec92982af32c285ffd
Antonio Gelameris <toniogela89@gmail.com>
2021-07-06 01:01:11 +0000

    Update decline-effect to 2.1.0

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   val coreDependencies: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-effect"    % "3.1.1",
-    "com.monovore"  %% "decline-effect" % "2.0.0",
+    "com.monovore"  %% "decline-effect" % "2.1.0",
     "is.cir"        %% "ciris"          % "2.0.1"
   )
 
