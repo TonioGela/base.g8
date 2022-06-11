e117ebca7e418833dd87d8746a754493513048aa
Antonio Gelameris <toniogela89@gmail.com>
2022-06-11 00:21:21 +0000

    Update fs2-core to 3.2.8

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     "org.typelevel"                %% "cats-effect"    % "3.3.12",
     "com.monovore"                 %% "decline-effect" % "2.2.0",
     "is.cir"                       %% "ciris"          % "2.3.2",
-    $if(add_fs2.truthy) $ "co.fs2" %% "fs2-core"       % "3.2.7" $endif$
+    $if(add_fs2.truthy) $ "co.fs2" %% "fs2-core"       % "3.2.8" $endif$
   )
 
   val testDependencies: Seq[ModuleID] = Seq("org.typelevel" %% "munit-cats-effect-3" % "1.0.7").map(_ % Test)
