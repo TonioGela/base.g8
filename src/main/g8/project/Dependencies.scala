616e3f9126a99ca10a21a2d3d98fe8f889b85f6a
Antonio Gelameris <toniogela89@gmail.com>
2022-07-16 00:23:53 +0000

    Update ciris to 2.3.3

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val coreDependencies: Seq[ModuleID] = Seq(
     "org.typelevel"                %% "cats-effect"    % "3.3.14",
     "com.monovore"                 %% "decline-effect" % "2.3.0",
-    "is.cir"                       %% "ciris"          % "2.3.2",
+    "is.cir"                       %% "ciris"          % "2.3.3",
     $if(add_fs2.truthy) $ "co.fs2" %% "fs2-core"       % "3.2.10" $endif$
   )
 
