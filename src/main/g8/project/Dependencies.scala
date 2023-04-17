975e852f3384b30ed52a0f1664090ddb511e3daa
Antonio Gelameris <toniogela89@gmail.com>
2023-04-17 00:19:38 +0000

    Update cats-effect to 3.4.9

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object Dependencies {
 
   val coreDependencies: Seq[ModuleID] = Seq(
-    "org.typelevel"                %% "cats-effect"    % "3.4.8",
+    "org.typelevel"                %% "cats-effect"    % "3.4.9",
     "com.monovore"                 %% "decline-effect" % "2.4.1",
     "is.cir"                       %% "ciris"          % "3.1.0",
     $if(add_fs2.truthy) $ "co.fs2" %% "fs2-core"       % "3.6.1" $endif$
